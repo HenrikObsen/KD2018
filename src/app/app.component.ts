e6566779ff88937434beb4d5de5cbae1e823113c
Henrik Obsen <hto@e-train.dk>
2018-01-16 14:08:52 +0100

    layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss','./css/font-awesome.min.css']
 })
 export class AppComponent {
   title = 'app';
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+
+export class HomeComponent implements OnInit {
+
+ 
+pause = 4000; //Pause inden nyt billede vises. Angives i millisekunder. 
+img;
+imgID = 0;
+rotator() {
+  var imgString = "";
+
+  imgString = '../assets/SliderImg/' + this.img[this.imgID];
+
+  if (this.imgID == this.img.length - 1) {
+    this.imgID = 0;
+  }
+  else {
+    this.imgID++;
+  }
+
+
+  $("#slider").fadeTo(700, 0, function () {
+      $("#slider").attr("alt", imgString);
+      $("#slider").attr("src", imgString);
+      $("#slider").fadeTo(700, 1, function () {
+
+      });
+
+  });
+
+  setTimeout(() => this.rotator(), this.pause);
+
+  //setTimeout(this.rotator(), this.pause);
+}
+
+loadImages(images) {
+
+    this.img = images.split(",");
+    this.imgID = Math.round(Math.random() * (this.img.length - 1));
+    this.rotator();
+}
+
+
+   constructor(){
+     this.loadImages('1.jpg,2.jpg,3.jpg');
+   }       
+
+   ngOnInit(){
+
+   }
+
+}
