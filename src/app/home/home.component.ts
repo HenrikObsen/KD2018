eda09c1ff1f1e1501d8aa70fd7965aba539b2351
Henrik Obsen <hto@e-train.dk>
2018-01-30 13:41:42 +0100

    bxslider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import '../js/jquery.bxslider.js';
 declare var jquery:any;
 declare var $ :any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss','../css/font-awesome.min.css'],
+  styleUrls: ['./home.component.scss','../css/font-awesome.min.css', '../css/jquery.bxslider.css'],
   encapsulation: ViewEncapsulation.None
 })
 
@@ -70,49 +71,19 @@ export class HomeComponent implements OnInit {
       content: "Kattagat Dykkerne Grenaa<br>att: Kristoffer Sørensen<br> Skakkesholm 20<br>DK-8500 Grenaa<br><br>formanden@kattagatdykkerne.dk<br>Tlf: 21702540"
     }
   ]; 
-pause = 4000; //Pause inden nyt billede vises. Angives i millisekunder. 
-img;
-imgID = 0;
-rotator() {
-  var imgString = "";
-
-  imgString = '../assets/SliderImg/' + this.img[this.imgID];
-
-  if (this.imgID == this.img.length - 1) {
-    this.imgID = 0;
-  }
-  else {
-    this.imgID++;
-  }
-
-
-  $("#slider").fadeTo(700, 0, function () {
-      $("#wrSlider").attr("style", "background-image: url('" + imgString + "')");
-  
-      $("#slider").fadeTo(700, 1, function () {
-
-      });
-
-  });
-
-  setTimeout(() => this.rotator(), this.pause);
-
-  //setTimeout(this.rotator(), this.pause);
-}
-
-loadImages(images) {
-
-    this.img = images.split(",");
-    this.imgID = Math.round(Math.random() * (this.img.length - 1));
-    this.rotator();
-}
 
 
    constructor(){
-     this.loadImages('5.jpg,6.jpg,7.jpg,8.jpg,9.jpg');
+     
    }       
 
    ngOnInit(){
+   
+      $('.bxslider').bxSlider({
+        auto: true,
+        mode: 'fade',
+      }
+    );
 
    }
 
